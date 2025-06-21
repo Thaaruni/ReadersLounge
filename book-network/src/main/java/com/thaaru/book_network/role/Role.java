7a5e026f0916c6af7bfaa69c61b78aaacfb67150
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-06-21 20:15:13 +0530

    Create user and its tokens

---
+++ book-network/src/main/java/com/thaaru/book_network/role/Role.java
@@ -0,0 +1,38 @@
+package com.thaaru.book_network.role;
+
+import com.thaaru.book_network.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime lastLogin;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+}
--- book-network/src/main/java/com/thaaru/book_network/user/User.java
+++ book-network/src/main/java/com/thaaru/book_network/user/User.java
@@ -1,50 +1,101 @@
 package com.thaaru.book_network.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.thaaru.book_network.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@Table(name = "_user")
+@EntityListeners(AuditingEntityListener.class)
 public class User implements UserDetails, Principal {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String lastName;
+    private String firstName;
+    private String dateOfBirth;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private boolean accountLocked;
+    private boolean enabled;
+
+    @ManyToMany(fetch=FetchType.EAGER)
+    @JsonIgnore //Ignore serializable of this
+    private List<Role> roles;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime lastLogin;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
     @Override
-    public String getName() {
-        return "";
+    public String getName(){
+        return email;
     }
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return this.roles
+                .stream().map(r -> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return !accountLocked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return enabled;
+    }
+
+    private String fullName(){
+        return firstName + " " + lastName;
     }
 }
