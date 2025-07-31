fdfea99c8a2d0d1faead8063b74dc26d88de363f
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-07-31 12:44:43 +0530

    Implement GlobalExceptionHandler and ExceptionResponse class for consistent error handling

---
+++ book-network/src/main/java/com/thaaru/book_network/handler/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.thaaru.book_network.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+        public ResponseEntity<ExceptionResponse> handleException(LockedException exp){
+
+        }
+
+}
