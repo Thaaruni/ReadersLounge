89f83b6048aa728a18c6ff10436dde70b481f8a9
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-08-01 16:43:28 +0530

    add custom exception handler for OperationNotPermittedException

---
+++ book-network/src/main/java/com/thaaru/book_network/exception/OperationNotPermittedException.java
@@ -0,0 +1,9 @@
+package com.thaaru.book_network.exception;
+
+public class OperationNotPermittedException extends RuntimeException {
+
+    public OperationNotPermittedException(String message) {
+        super(message);
+    }
+}
+
