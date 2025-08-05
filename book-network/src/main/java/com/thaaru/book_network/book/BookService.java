4cf2a80f41c5ea603cb819e0dcc519022ccc0a4f
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-08-05 12:55:02 +0530

    Implement file upload logic

--- book-network/src/main/java/com/thaaru/book_network/book/BookService.java
+++ book-network/src/main/java/com/thaaru/book_network/book/BookService.java
@@ -209,7 +209,7 @@ public class BookService {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
         User user = ((User) connectedUser.getPrincipal());
-        var bookCover = fileStorageService.saveFile(file, book.getId());
+        var bookCover = fileStorageService.saveFile(file, user.getId());
         book.setBookCover(bookCover);
         bookRepository.save(book);
     }
--- book-network/src/main/java/com/thaaru/book_network/book/file/FileStorageService.java
+++ book-network/src/main/java/com/thaaru/book_network/book/file/FileStorageService.java
@@ -9,11 +9,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static java.io.File.separator;
+
 @Service
+@Slf4j //for get some logs
 @RequiredArgsConstructor
 public class FileStorageService {
 
@@ -22,13 +26,50 @@ public class FileStorageService {
 
     public String saveFile(
             @Nonnull MultipartFile sourceFile,
-            @Nonnull Book book,
             @Nonnull Integer userId
     ) {
-        final String fileUploadSubPath = "users" + File.separator + userId;
+        final String fileUploadSubPath = "users" + separator + userId;
         return uploadFile(sourceFile, fileUploadSubPath);
     }
 
+    private String uploadFile(
+            @Nonnull MultipartFile sourceFile,
+            @Nonnull String fileUploadSubPath
+    ) {
+        final String finalUploadPath = fileUploadPath + separator + fileUploadSubPath;
+        File targetFolder = new File(finalUploadPath);
 
+        if (!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if (!folderCreated) {
+                log.warn("Failed to create the target folder: " + targetFolder);
+                return null;
+            }
+        }
+        final String fileExtension = getFileExtension(sourceFile.getOriginalFilename());
+        //upload the files like ./upload/users/1/12345678854.jpg
+        String targetFilePath = finalUploadPath + separator + System.currentTimeMillis() + "." + fileExtension;
+        Path targetPath = Paths.get(targetFilePath);
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("File saved to: " + targetFilePath);
+            return targetFilePath;
+        } catch (IOException e) {
+            log.error("File was not saved", e);
+        }
+        return null;
+    }
+
+    private String getFileExtension(String fileName) {
+        if (fileName == null || fileName.isEmpty()) {
+            return "";
+        }
+        int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex == -1) {
+            return "";
+        }
+        //if file has .JPG --> jpg
+        return fileName.substring(lastDotIndex + 1).toLowerCase();
+    }
 }
 
