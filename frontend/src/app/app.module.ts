68ee69e3c17f9c2bbcfbc842a2caf96e92176e42
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-08-07 21:49:20 +0530

    Added RouterModule

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule
+  ],
+  providers: [HttpClient],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
