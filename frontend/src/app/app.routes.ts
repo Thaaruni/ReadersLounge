f41abdb4254281833ba616859ccca460d2fce8b9
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-08-08 14:24:04 +0530

    Remove frontend directory

--- frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-// app/app.routes.ts
-import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent }
-];
