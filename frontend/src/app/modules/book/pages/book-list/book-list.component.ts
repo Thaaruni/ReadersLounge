93af4d29c8be621f13d7960d05f1ec3b7d8f11cf
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-08-09 17:32:27 +0530

    Implement book list layout with pagination and book card events

--- frontend/src/app/modules/book/pages/book-list/book-list.component.ts
+++ frontend/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -37,4 +37,55 @@ export class BookListComponent implements OnInit {
       });
   }
 
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page --;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.bookResponse.totalPages as number - 1;
+  }
+
+  borrowBook(book: BookResponse) {
+    this.message = '';
+    this.level = 'success';
+    this.bookService.borrowBook({
+      'book-id': book.id as number
+    }).subscribe({
+      next: () => {
+        this.level = 'success';
+        this.message = 'Book successfully added to your list';
+      },
+      error: (err) => {
+        console.log(err);
+        this.level = 'error';
+        this.message = err.error.error;
+      }
+    });
+  }
+
+  displayBookDetails(book: BookResponse) {
+    this.router.navigate(['books', 'details', book.id]);
+  }
+
 }
