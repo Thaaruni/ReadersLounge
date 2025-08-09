4ab57b3386c9a1194e3e91510ba5f8451832efa2
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-08-09 18:50:53 +0530

    Create component for manage books

---
+++ frontend/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-book',
+  standalone: false,
+  templateUrl: './manage-book.component.html',
+  styleUrl: './manage-book.component.scss'
+})
+export class ManageBookComponent {
+
+}
