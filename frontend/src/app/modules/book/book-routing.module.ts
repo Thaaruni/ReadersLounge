ec3997e0073a3cbf1fda87481c150f3f6ee70ffc
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-08-10 15:03:55 +0530

    Add Keycloak service and authentication guard

--- frontend/src/app/modules/book/book-routing.module.ts
+++ frontend/src/app/modules/book/book-routing.module.ts
@@ -5,32 +5,39 @@ import {BookListComponent} from './pages/book-list/book-list.component';
 import {MyBooksComponent} from './pages/my-books/my-books.component';
 import {ManageBookComponent} from './pages/manage-book/manage-book.component';
 import {BorrowedBookListComponent} from './pages/borrowed-book-list/borrowed-book-list.component';
-import {ReturnBooksComponent} from './pages/returned-books/return-books.component';
+import {ReturnedBooksComponent} from './pages/returned-books/returned-books.component';
+import {authGuard} from '../../services/guard/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    canActivate: [authGuard],
     children: [
       {
         path: '',
-        component: BookListComponent
+        component: BookListComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'my-books',
-        component: MyBooksComponent
+        component: MyBooksComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'manage',
-        component: ManageBookComponent
+        component: ManageBookComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'my-borrowed-books',
-        component: BorrowedBookListComponent
+        component: BorrowedBookListComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'my-returned-books',
-        component: ReturnBooksComponent
+        component: ReturnedBooksComponent,
+        canActivate: [authGuard]
       }
     ]
   }
