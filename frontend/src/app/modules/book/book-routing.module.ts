cbad89b0b1b3532a3ede08d6dd757495d2cfa34c
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-08-10 13:10:10 +0530

    Create component for return books

--- frontend/src/app/modules/book/book-routing.module.ts
+++ frontend/src/app/modules/book/book-routing.module.ts
@@ -4,6 +4,8 @@ import {MainComponent} from './pages/main/main.component';
 import {BookListComponent} from './pages/book-list/book-list.component';
 import {MyBooksComponent} from './pages/my-books/my-books.component';
 import {ManageBookComponent} from './pages/manage-book/manage-book.component';
+import {BorrowedBookListComponent} from './pages/borrowed-book-list/borrowed-book-list.component';
+import {ReturnBooksComponent} from './pages/return-books/return-books.component';
 
 const routes: Routes = [
   {
@@ -19,8 +21,16 @@ const routes: Routes = [
         component: MyBooksComponent
       },
       {
-        path: 'manage/:bookId',
+        path: 'manage',
         component: ManageBookComponent
+      },
+      {
+        path: 'my-borrowed-books',
+        component: BorrowedBookListComponent
+      },
+      {
+        path: 'my-returned-books',
+        component: ReturnBooksComponent
       }
     ]
   }
