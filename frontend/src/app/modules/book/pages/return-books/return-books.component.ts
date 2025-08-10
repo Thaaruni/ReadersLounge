689c260b9fe87c156583deecea5d6d848c773bc4
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-08-10 13:49:00 +0530

    Add returned books view with approval action in template

--- frontend/src/app/modules/book/pages/return-books/return-books.component.ts
+++ frontend/src/app/modules/book/pages/return-books/return-books.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PageResponseBorrowedBookResponse} from '../../../../services/models/page-response-borrowed-book-response';
+import {BookService} from '../../../../services/services/book.service';
+import {BorrowedBookResponse} from '../../../../services/models/borrowed-book-response';
 
 @Component({
-  selector: 'app-return-books',
-  standalone: false,
-  templateUrl: './return-books.component.html',
-  styleUrl: './return-books.component.scss'
+  selector: 'app-returned-books',
+  templateUrl: './returned-books.component.html',
+  styleUrls: ['./returned-books.component.scss']
 })
-export class ReturnBooksComponent {
+export class ReturnedBooksComponent implements OnInit {
+
+  page = 0;
+  size = 5;
+  pages: any = [];
+  returnedBooks: PageResponseBorrowedBookResponse = {};
+  message = '';
+  level: 'success' |'error' = 'success';
+  constructor(
+    private bookService: BookService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.findAllReturnedBooks();
+  }
+
+  private findAllReturnedBooks() {
+    this.bookService.findAllReturnedBooks({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (resp) => {
+        this.returnedBooks = resp;
+        this.pages = Array(this.returnedBooks.totalPages)
+          .fill(0)
+          .map((x, i) => i);
+      }
+    });
+  }
+
 
 }
